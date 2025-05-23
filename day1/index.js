009e30dd73f8f07d303c1d05f00272955eb4dd2d
GurmeetSingh0289 <gurmeet0289singh@gmail.com>
2025-05-23 19:31:11 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,25 @@
+//loging string to the console
+console.log("Hello World");     //output:Hello World
+var num=20;
+console.log(num);             //output:20
+//  it can be reassigned and redeclared also
+num=50;
+console.log(num);               //output=50
+
+let a=40;
+console.log(a);                 //output=40
+// let can be reassigned but not redeclared
+a=80;
+console.log(a);                 //output=80
+const b=10;
+console.log(b);                 //outpur=10
+// const cannot be reassigned and redeclared
+ b=20;
+console.log(b);                   //outpur=show error
+
+/*note:     
+Semicolon (;) is not mandatory in JavaScript,but are recommended.
+ there are 3 keywords to declare a variable i.e-----var,let,const  
+ var is global-scope varibale
+ let and const are block-scope variable
+ */
