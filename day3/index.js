6821c823e5543a482388fecd6cd45dfa83644267
GurmeetSingh0289 <gurmeet0289singh@gmail.com>
2025-05-26 23:51:08 +0530

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,20 @@
+let a=11;
+console.log(a,a,a,a--,a--,a--,a++,a++,a++);
+//output:11 11 11 11 10 9 8 9 10
+
+
+  let b=45;
+  console.log(b,b++,b++,b++,++b,++b,++b,b);
+//output:45 45 46 47 49 50 51 51
+
+
+let c=15;
+c=+10;
+console.log(c);
+//output:10
+
+
+let d=15;
+d+=10;
+console.log(d);
+//output:25
