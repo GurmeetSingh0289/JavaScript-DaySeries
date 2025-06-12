3bcdc00517b49bb40fb756edc7592a116ba39472
GurmeetSingh0289 <gurmeet0289singh@gmail.com>
2025-06-12 08:20:26 +0530

    Add files via upload

---
+++ day7/index.js
@@ -0,0 +1,27 @@
+// string in javascript 
+let str="Hello World";
+// methods...................
+let sp=str.split(" ");   //split method
+console.log(sp);
+let res000=str.trim();   //trim method 
+console.log(res000);
+let res00=str.replace("Hello", "Hey..");   //replace method
+console.log(res00);
+let res0=str.slice(2,8)   //slice method
+console.log(res0);
+let res=str.indexOf("World");       //indexOf method
+console.log(res);
+let res1=str.lastIndexOf("World");   //lastIndexOf method
+console.log(res1);
+let str1="prep";
+let str2="byte";
+console.log(str1.concat(str2));      //concat method
+console.log(str2.concat(str1));
+let about="hii my name is gurmeet singh";
+let newMessage=about.substring(7,15)    //substring method
+console.log(newMessage);
+
+
+
+
+
